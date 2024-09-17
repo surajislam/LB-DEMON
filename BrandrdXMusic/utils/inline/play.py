5a61da51c6037c92bf5beeb3559fac45954690db
surajislam <154444093+surajislam@users.noreply.github.com>
2024-09-17 14:22:03 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_SB_SIMPLE_II"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/MR_D_BLACK_DEMON"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/SDK_GIVEWAY"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/9NyYJ_Sll_9mYjM1"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_SB_SIMPLE_II"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/MR_D_BLACK_DEMON"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/SKD_GROUP_2"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/9NyYJ_Sll_9mYjM1"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -245,10 +245,10 @@ def stream_markup2(_, chat_id):
         ],
 [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_SIMPLE_II"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/MR_D_BLACK_DEMON"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/SKD_GROUP_2"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/9NyYJ_Sll_9mYjM1"
             ),
         ],
 
